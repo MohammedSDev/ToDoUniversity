268741cf1e6925904b85aed0e4fe76ad9eea2b1a
Mohammed Salem <m.salem@clickapps.co>
2018-09-27 18:36:44 +0530

    git init

---
+++ app/src/main/java/com/university/todo/LockActivity.java
@@ -0,0 +1,18 @@
+package com.university.todo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+public class LockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lock);
+
+
+    }
+
+}
